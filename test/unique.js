bd9814c315e2dae06c3ea1e7b711afe595cee63b
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-27 11:55:53 +0000

    add test for unique

---
+++ test/unique.js
@@ -0,0 +1,28 @@
+
+var pull = require('../')
+var test = require('tape')
+
+test('unique', function (t) {
+  var numbers = [1, 2, 2, 3, 4, 5, 6, 4, 0, 6, 7, 8, 3, 1, 2, 9, 0]
+
+  pull.values(numbers)
+  .pipe(pull.unique())
+  .pipe(pull.collect(function (err, ary) {
+    t.deepEqual(ary.sort(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    t.end()
+  }))
+
+})
+
+test('non-unique', function (t) {
+  var numbers = [1, 2, 2, 3, 4, 5, 6, 4, 0, 6, 7, 8, 3, 1, 2, 9, 0]
+
+  pull.values(numbers)
+  .pipe(pull.nonUnique())
+  .pipe(pull.collect(function (err, ary) {
+    t.deepEqual(ary.sort(), [0, 1, 2, 2, 3, 4, 6])
+    t.end()
+  }))
+
+
+})
