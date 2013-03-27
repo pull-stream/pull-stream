d915d320e508d64fcc70da3a4fc9c9d04e475067
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-27 13:29:59 +0000

    implement group

--- throughs.js
+++ throughs.js
@@ -93,6 +93,35 @@ var nonUnique = exports.nonUnique = function (read, field) {
   return unique(read, field, true)
 }
 
+var group = exports.group =
+function (read, size) {
+  var ended; size = size || 5
+  var queue = []
+
+  return function (end, cb) {
+    //this means that the upstream is sending an error.
+    if(end) return read(ended = end, cb)
+    //this means that we read an end before.
+    if(ended) return cb(ended)
+
+    read(null, function next(end, data) {
+      if(ended = ended || end) {
+        if(!queue.length)
+          return cb(ended)
+
+        var _queue = queue; queue = []
+        return cb(null, _queue)
+      }
+      queue.push(data)
+      if(queue.length < size)
+        return read(null, next)
+
+      var _queue = queue; queue = []
+      cb(null, _queue)
+    })
+  }
+}
+
 var nextTick = process.nextTick
 
 var highWaterMark = exports.highWaterMark = 
