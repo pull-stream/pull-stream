6a34440585236593b04f29d50c3fe07255692890
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-27 13:33:47 +0000

    logging it tests

--- test/group.js
+++ test/group.js
@@ -8,11 +8,14 @@ require('tape')('group', function (t) {
   .pipe(function (read) {
     return function (end, cb) {
       read(null, function (end, data) {
-        if(!end) t.deepEqual(data, [
-          [0, 1,2,3,4,5,6],
-          [7,8,9,10,11,12,13],
-          [14, 15,16,17,18,19]
-        ])
+        if(!end) {
+          t.deepEqual(data, [
+            [0, 1,2,3,4,5,6],
+            [7,8,9,10,11,12,13],
+            [14, 15,16,17,18,19]
+          ])
+          console.log(data)
+        }
        process.nextTick(cb.bind(null, end, data))
       })
     }
