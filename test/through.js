16af48ffcff9799ac3ba268d5121850e978bd204
Dominic Tarr <dominic.tarr@gmail.com>
2013-04-04 23:10:51 +0100

    take ends the source first

--- test/through.js
+++ test/through.js
@@ -7,7 +7,9 @@ require('tape')('through - onEnd', function (t) {
   .pipe(pull.through(null, function (err) {
     console.log('end')
     t.ok(true)
-    t.end()
+    process.nextTick(function () {
+      t.end()
+    })
   }))
   .pipe(pull.take(10))
   .pipe(pull.collect(function (err, ary) {
