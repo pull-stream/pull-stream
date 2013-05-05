5f1a1047fd206aafe8d09749bcde2ffbf78cc7b1
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-05 01:09:27 +0100

    test for paraMap

--- test/async-map.js
+++ test/async-map.js
@@ -14,3 +14,30 @@ require('tape')('async-map', function (t) {
   }))
 
 })
+
+require('tape')('para-map', function (t) {
+
+  var n = 0, m = 0, w = 6, i = 0
+  pull.count()
+  .pipe(pull.take(21))
+  .pipe(pull.paraMap(function (data, cb) {
+    console.log('>', i++)
+    n ++
+    m = Math.max(m, n)
+    setTimeout(function () {
+      n --
+
+      console.log('<')
+      cb(null, data + 1)
+    }, Math.random() * 20)
+  }, w))
+  .pipe(pull.collect(function (err, ary) {
+    console.log(ary)
+    t.equal(ary.length, 21)
+    t.equal(m, w)
+    t.end()
+  }))
+
+})
+
+
