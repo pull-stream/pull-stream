91e1d0385591b93192c64b806deb869acf40e88f
Dominic Tarr <dominic.tarr@gmail.com>
2014-04-15 10:40:32 +1000

    handle duplex streams

--- index.js
+++ index.js
@@ -3,24 +3,54 @@ var sinks    = require('./sinks')
 var throughs = require('./throughs')
 var u        = require('pull-core')
 
-function isThrough (fun) {
-  return fun.type === "Through" || fun.length === 1
+function isFunction (fun) {
+  return 'function' === typeof fun
 }
 
+function isReader (fun) {
+  return fun && (fun.type === "Through" || fun.length === 1)
+}
 var exports = module.exports = function pull () {
   var args = [].slice.call(arguments)
 
-  if(isThrough(args[0]))
+  if(isReader(args[0]))
     return function (read) {
       args.unshift(read)
       return pull.apply(null, args)
     }
 
   var read = args.shift()
+
+  //if the first function is a duplex stream,
+  //pipe from the source.
+  if(isFunction(read.source))
+    read = read.source
+
+  function next () {
+    var s = args.shift()
+
+    if(null == s)
+      return next()
+
+    if(isFunction(s)) return s
+
+    return function (read) {
+      s.sink(read)
+      //this supports pipeing through a duplex stream
+      //pull(a, b, a) "telephone style".
+      //if this stream is in the a (first & last position)
+      //s.source will have already been used, but this should never be called
+      //so that is okay.
+      return s.source
+    }
+  }
+
   while(args.length)
-    read = args.shift() (read)
+    read = next() (read)
+
   return read
 }
+
 
 for(var k in sources)
   exports[k] = u.Source(sources[k])
