dc506752b973dedf7951eba7a4d21c574bcfde73
Dominic Tarr <dominic.tarr@gmail.com>
2015-06-07 18:19:33 +0200

    test pull.values abort

--- test/values.js
+++ test/values.js
@@ -25,3 +25,23 @@ tape('values - object', function (t) {
   )
 
 })
+
+tape('values, abort', function (t) {
+
+  t.plan(3)
+
+  var err = new Error('intentional')
+
+  var read = pull.values([1,2,3], function (err) {
+    t.end()
+  })
+
+  read(null, function (_, one) {
+    t.notOk(_)
+    t.equal(one, 1)
+    read(err, function (_err) {
+      t.equal(_err, err)
+    })
+  })
+
+})
