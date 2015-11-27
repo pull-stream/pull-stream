c8c1db28a6449a384bc19c963fa13ca25fd86dea
Dominic Tarr <dominic.tarr@gmail.com>
2015-11-27 21:28:15 +1300

    remove unnecessary cleverness

--- sinks.js
+++ sinks.js
@@ -1,40 +1,107 @@
-var drain = exports.drain = function (read, op, done) {
+'use strict'
 
-  ;(function next() {
-    var loop = true, cbed = false
-    while(loop) {
-      cbed = false
-      read(null, function (end, data) {
-        cbed = true
-        if(end) {
+function id (item) { return item }
+
+function prop (key) {
+  return (
+    'string' == typeof key
+    ? function (data) { return data[key] }
+    : 'object' === typeof key && 'function' === typeof key.exec //regexp
+    ? function (data) { var v = map.exec(data); return v && v[0] }
+    : key || id
+  )
+}
+
+
+var drain = exports.drain = function (op, done) {
+
+  return function (read) {
+
+    //this function is much simpler to write if you
+    //just use recursion, but by using a while loop
+    //we do not blow the stack if the stream happens to be sync.
+    ;(function next() {
+      var loop = true, cbed = false
+      while(loop) {
+        cbed = false
+        read(null, function (end, data) {
+          cbed = true
+          if(end) {
+            loop = false
+            if(done) done(end === true ? null : end)
+            else if(end && end !== true)
+              throw end
+          }
+          else if(op && false === op(data)) {
+            loop = false
+            read(true, done || function () {})
+          }
+          else if(!loop){
+            next()
+          }
+        })
+        if(!cbed) {
           loop = false
-          if(done) done(end === true ? null : end)
-          else if(end && end !== true)
-            throw end
+          return
         }
-        else if(op && false === op(data)) {
-          loop = false
-          read(true, done || function () {})
-        }
-        else if(!loop){
-          next()
-        }
-      })
-      if(!cbed) {
-        loop = false
-        return
       }
-    }
-  })()
+    })()
+
+  }
 }
 
-var onEnd = exports.onEnd = function (read, done) {
-  return drain(read, null, done)
+var onEnd = exports.onEnd = function (done) {
+  return drain(null, done)
 }
 
-var log = exports.log = function (read, done) {
-  return drain(read, function (data) {
+var log = exports.log = function (done) {
+  return drain(function (data) {
     console.log(data)
   }, done)
+}
+
+var find =
+exports.find = function (test, cb) {
+  var ended = false
+  if(!cb)
+    cb = test, test = id
+  else
+    test = prop(test) || id
+
+  return drain(function (data) {
+    if(test(data)) {
+      ended = true
+      cb(null, data)
+    return false
+    }
+  }, function (err) {
+    if(ended) return //already called back
+    cb(err === true ? null : err, null)
+  })
+}
+
+var reduce = exports.reduce = function (reduce, acc, cb) {
+
+  return drain(function (data) {
+    acc = reduce(acc, data)
+  }, function (err) {
+    cb(err, acc)
+  })
+
+}
+
+var collect = exports.collect =
+function (cb) {
+  return reduce(function (arr, item) {
+    arr.push(item)
+    return arr
+  }, [], cb)
+}
+
+var concat = exports.concat =
+function (cb) {
+  return reduce(function (a, b) {
+    return a + b
+  }, '', cb)
 }
 
--- throughs.js
+++ throughs.js
@@ -1,135 +1,115 @@
-var u      = require('pull-core')
+'use strict';
+
+function id (item) { return item }
+
+function prop (key) {
+  return (
+    'string' == typeof key
+    ? function (data) { return data[key] }
+    : 'object' === typeof key && 'function' === typeof key.exec //regexp
+    ? function (data) { var v = map.exec(data); return v && v[0] }
+    : key
+  )
+}
+
+function tester (test) {
+  return (
+    'object' === typeof test && 'function' === typeof test.test //regexp
+    ? function (data) { return test.test(data) }
+    : prop (test) || id
+  )
+}
+
 var sources = require('./sources')
 var sinks = require('./sinks')
 
-var prop   = u.prop
-var id     = u.id
-var tester = u.tester
-
 var map = exports.map =
-function (read, map) {
-  map = prop(map) || id
-  return function (abort, cb) {
-    read(abort, function (end, data) {
-      try {
-      data = !end ? map(data) : null
-      } catch (err) {
-        return read(err, function () {
-          return cb(err)
-        })
-      }
-      cb(end, data)
-    })
+function (map) {
+  if(!map) return id
+  map = prop(map)
+  return function (read) {
+    return function (abort, cb) {
+      read(abort, function (end, data) {
+        try {
+        data = !end ? map(data) : null
+        } catch (err) {
+          return read(err, function () {
+            return cb(err)
+          })
+        }
+        cb(end, data)
+      })
+    }
   }
 }
 
 var asyncMap = exports.asyncMap =
-function (read, map) {
-  if(!map) return read
-  return function (end, cb) {
-    if(end) return read(end, cb) //abort
-    read(null, function (end, data) {
-      if(end) return cb(end, data)
-      map(data, cb)
-    })
-  }
-}
-
-var paraMap = exports.paraMap =
-function (read, map, width) {
-  if(!map) return read
-  var ended = false, queue = [], _cb
-
-  function drain () {
-    if(!_cb) return
-    var cb = _cb
-    _cb = null
-    if(queue.length)
-      return cb(null, queue.shift())
-    else if(ended && !n)
-      return cb(ended)
-    _cb = cb
-  }
-
-  function pull () {
-    read(null, function (end, data) {
-      if(end) {
-        ended = end
-        return drain()
-      }
-      n++
-      map(data, function (err, data) {
-        n--
-
-        queue.push(data)
-        drain()
+function (map) {
+  if(!map) return id //when read is passed, pass it on.
+  return function (read) {
+    return function (end, cb) {
+      if(end) return read(end, cb) //abort
+      read(null, function (end, data) {
+        if(end) return cb(end, data)
+        map(data, cb)
       })
-
-      if(n < width && !ended)
-        pull()
-    })
+    }
   }
-
-  var n = 0
-  return function (end, cb) {
-    if(end) return read(end, cb) //abort
-    //continue to read while there are less than 3 maps in flight
-    _cb = cb
-    if(queue.length || ended)
-      pull(), drain()
-    else pull()
-  }
-  return highWaterMark(asyncMap(read, map), width)
 }
 
 var filter = exports.filter =
-function (read, test) {
+function (test) {
   //regexp
   test = tester(test)
-  return function next (end, cb) {
-    var sync, loop = true
-    while(loop) {
-      loop = false
-      sync = true
-      read(end, function (end, data) {
-        if(!end && !test(data))
-          return sync ? loop = true : next(end, cb)
-        cb(end, data)
-      })
-      sync = false
+  return function (read) {
+    return function next (end, cb) {
+      var sync, loop = true
+      while(loop) {
+        loop = false
+        sync = true
+        read(end, function (end, data) {
+          if(!end && !test(data))
+            return sync ? loop = true : next(end, cb)
+          cb(end, data)
+        })
+        sync = false
+      }
     }
   }
 }
 
 var filterNot = exports.filterNot =
-function (read, test) {
+function (test) {
   test = tester(test)
-  return filter(read, function (e) {
-    return !test(e)
-  })
+  return filter(function (data) { return !test(data) })
 }
 
+//a pass through stream that doesn't change the value.
 var through = exports.through =
-function (read, op, onEnd) {
+function (op, onEnd) {
   var a = false
+
   function once (abort) {
     if(a || !onEnd) return
     a = true
     onEnd(abort === true ? null : abort)
   }
 
-  return function (end, cb) {
-    if(end) once(end)
-    return read(end, function (end, data) {
-      if(!end) op && op(data)
-      else once(end)
-      cb(end, data)
-    })
+  return function (read) {
+    return function (end, cb) {
+      if(end) once(end)
+      return read(end, function (end, data) {
+        if(!end) op && op(data)
+        else once(end)
+        cb(end, data)
+      })
+    }
   }
 }
 
+//read a number of items and then stop.
 var take = exports.take =
-function (read, test, opts) {
+function (test, opts) {
   opts = opts || {}
   var last = opts.last || false // whether the first item for which !test(item) should still pass
   var ended = false
@@ -140,35 +120,39 @@ function (read, test, opts) {
     }
   }
 
-  function terminate (cb) {
-    read(true, function (err) {
-      last = false; cb(err || true)
-    })
-  }
+  return function (read) {
 
-  return function (end, cb) {
-    if(ended)            last ? terminate(cb) : cb(ended)
-    else if(ended = end) read(ended, cb)
-    else
-      read(null, function (end, data) {
-        if(ended = ended || end) {
-          //last ? terminate(cb) :
-          cb(ended)
-        }
-        else if(!test(data)) {
-          ended = true
-          last ? cb(null, data) : terminate(cb)
-        }
-        else
-          cb(null, data)
+    function terminate (cb) {
+      read(true, function (err) {
+        last = false; cb(err || true)
       })
+    }
+
+    return function (end, cb) {
+      if(ended)            last ? terminate(cb) : cb(ended)
+      else if(ended = end) read(ended, cb)
+      else
+        read(null, function (end, data) {
+          if(ended = ended || end) {
+            //last ? terminate(cb) :
+            cb(ended)
+          }
+          else if(!test(data)) {
+            ended = true
+            last ? cb(null, data) : terminate(cb)
+          }
+          else
+            cb(null, data)
+        })
+    }
   }
 }
 
-var unique = exports.unique = function (read, field, invert) {
+//drop items you have already seen.
+var unique = exports.unique = function (field, invert) {
   field = prop(field) || id
   var seen = {}
-  return filter(read, function (data) {
+  return filter(function (data) {
     var key = field(data)
     if(seen[key]) return !!invert //false, by default
     else seen[key] = true
@@ -176,173 +160,50 @@ var unique = exports.unique = function (read, field, invert) {
   })
 }
 
-var nonUnique = exports.nonUnique = function (read, field) {
-  return unique(read, field, true)
+//passes an item through when you see it for the second time.
+var nonUnique = exports.nonUnique = function (field) {
+  return unique(field, true)
 }
 
-var group = exports.group =
-function (read, size) {
-  var ended; size = size || 5
-  var queue = []
-
-  return function (end, cb) {
-    //this means that the upstream is sending an error.
-    if(end) return read(ended = end, cb)
-    //this means that we read an end before.
-    if(ended) return cb(ended)
-
-    read(null, function next(end, data) {
-      if(ended = ended || end) {
-        if(!queue.length)
-          return cb(ended)
-
-        var _queue = queue; queue = []
-        return cb(null, _queue)
+//convert a stream of arrays or streams into just a stream.
+var flatten = exports.flatten = function () {
+  return function (read) {
+    var _read
+    return function (abort, cb) {
+      if (abort) { //abort the current stream, and then stream of streams.
+        _read ? _read(abort, function(err) {
+          read(err || abort, cb)
+        }) : read(abort, cb)
       }
-      queue.push(data)
-      if(queue.length < size)
-        return read(null, next)
+      else if(_read) nextChunk()
+      else nextStream()
 
-      var _queue = queue; queue = []
-      cb(null, _queue)
-    })
-  }
-}
-
-var flatten = exports.flatten = function (read) {
-  var _read
-  return function (abort, cb) {
-    if (abort) {
-      _read ? _read(abort, function(err) {
-        read(err || abort, cb)
-      }) : read(abort, cb)
-    }
-    else if(_read) nextChunk()
-    else nextStream()
-
-    function nextChunk () {
-      _read(null, function (err, data) {
-        if (err === true) nextStream()
-        else if (err) {
-          read(true, function(abortErr) {
-            // TODO: what do we do with the abortErr?
-            cb(err)
-          })
-        }
-        else cb(null, data)
-      })
-    }
-    function nextStream () {
-      read(null, function (end, stream) {
-        if(end)
-          return cb(end)
-        if(Array.isArray(stream) || stream && 'object' === typeof stream)
-          stream = sources.values(stream)
-        else if('function' != typeof stream)
-          throw new Error('expected stream of streams')
-        _read = stream
-        nextChunk()
-      })
-    }
-  }
-}
-
-var prepend =
-exports.prepend =
-function (read, head) {
-
-  return function (abort, cb) {
-    if(head !== null) {
-      if(abort)
-        return read(abort, cb)
-      var _head = head
-      head = null
-      cb(null, _head)
-    } else {
-      read(abort, cb)
-    }
-  }
-
-}
-
-//var drainIf = exports.drainIf = function (op, done) {
-//  sinks.drain(
-//}
-
-var _reduce = exports._reduce = function (read, reduce, initial) {
-  return function (close, cb) {
-    if(close) return read(close, cb)
-    if(ended) return cb(ended)
-
-    sinks.drain(function (item) {
-      initial = reduce(initial, item)
-    }, function (err, data) {
-      ended = err || true
-      if(!err) cb(null, initial)
-      else     cb(ended)
-    })
-    (read)
-  }
-}
-
-var nextTick = process.nextTick
-
-var highWaterMark = exports.highWaterMark =
-function (read, highWaterMark) {
-  var buffer = [], waiting = [], ended, ending, reading = false
-  highWaterMark = highWaterMark || 10
-
-  function readAhead () {
-    while(waiting.length && (buffer.length || ended))
-      waiting.shift()(ended, ended ? null : buffer.shift())
-
-    if (!buffer.length && ending) ended = ending;
-  }
-
-  function next () {
-    if(ended || ending || reading || buffer.length >= highWaterMark)
-      return
-    reading = true
-    return read(ended || ending, function (end, data) {
-      reading = false
-      ending = ending || end
-      if(data != null) buffer.push(data)
-
-      next(); readAhead()
-    })
-  }
-
-  process.nextTick(next)
-
-  return function (end, cb) {
-    ended = ended || end
-    waiting.push(cb)
-
-    next(); readAhead()
-  }
-}
-
-var flatMap = exports.flatMap =
-function (read, mapper) {
-  mapper = mapper || id
-  var queue = [], ended
-
-  return function (abort, cb) {
-    if(queue.length) return cb(null, queue.shift())
-    else if(ended)   return cb(ended)
-
-    read(abort, function next (end, data) {
-      if(end) ended = end
-      else {
-        var add = mapper(data)
-        while(add && add.length)
-          queue.push(add.shift())
+      function nextChunk () {
+        _read(null, function (err, data) {
+          if (err === true) nextStream()
+          else if (err) {
+            read(true, function(abortErr) {
+              // TODO: what do we do with the abortErr?
+              cb(err)
+            })
+          }
+          else cb(null, data)
+        })
       }
-
-      if(queue.length) cb(null, queue.shift())
-      else if(ended)   cb(ended)
-      else             read(null, next)
-    })
+      function nextStream () {
+        _read = null
+        read(null, function (end, stream) {
+          if(end)
+            return cb(end)
+          if(Array.isArray(stream) || stream && 'object' === typeof stream)
+            stream = sources.values(stream)
+          else if('function' != typeof stream)
+            throw new Error('expected stream of streams')
+          _read = stream
+          nextChunk()
+        })
+      }
+    }
   }
 }
 
