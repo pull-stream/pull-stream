c2914e0006d7eb737a47f90c1dc2b514fe3f91f0
Dominic Tarr <dominic.tarr@gmail.com>
2015-11-27 19:55:58 +1300

    update tests to remove coverage for removed parts

--- test/async-map.js
+++ test/async-map.js
@@ -2,17 +2,6 @@ var pull = require('../')
 
 require('tape')('async-map', function (t) {
 
-//  pull.count()
-//  .pipe(pull.take(21))
-//  .pipe(pull.asyncMap(function (data, cb) {
-//    return cb(null, data + 1)
-//  }))
-//  .pipe(pull.collect(function (err, ary) {
-//    console.log(ary)
-//    t.equal(ary.length, 21)
-//    t.end()
-//  }))
-
   pull(
     pull.count(),
     pull.take(21),
@@ -26,33 +15,6 @@ require('tape')('async-map', function (t) {
     })
   )
 
-
-})
-
-require('tape')('para-map', function (t) {
-
-  var n = 0, m = 0, w = 6, i = 0
-  pull(
-    pull.count(),
-    pull.take(21),
-    pull.paraMap(function (data, cb) {
-      console.log('>', i++)
-      n ++
-      m = Math.max(m, n)
-      setTimeout(function () {
-        n --
-
-        console.log('<')
-        cb(null, data + 1)
-      }, Math.random() * 20)
-    }, w),
-    pull.collect(function (err, ary) {
-      console.log(ary)
-      t.equal(ary.length, 21)
-      t.equal(m, w)
-      t.end()
-    })
-  )
 
 })
 
--- test/compose.js
+++ test/compose.js
@@ -6,19 +6,21 @@ test('join through streams with pipe', function (t) {
 
   var map = pull.map
 
-  var pipeline = 
-    map(function (d) {
-      //make exciting!
-      return d + '!'
-    })
-    .pipe(map(function (d) {
-      //make loud
-      return d.toUpperCase()
-    }))
-    .pipe(map(function (d) {
-      //add sparkles
-      return '*** ' + d + ' ***'
-    }))
+  var pipeline =
+    pull(
+      map(function (d) {
+        //make exciting!
+        return d + '!'
+      }),
+      map(function (d) {
+        //make loud
+        return d.toUpperCase()
+      }),
+      map(function (d) {
+        //add sparkles
+        return '*** ' + d + ' ***'
+      })
+    )
   //the pipe line does not have a source stream.
   //so it should be a reader (function that accepts
   //a read function)
@@ -29,23 +31,27 @@ test('join through streams with pipe', function (t) {
   //if we pipe a read function to the pipeline,
   //the pipeline will become readable!
 
-  var read = 
-    pull.readArray(['billy', 'joe', 'zeke'])
-    .pipe(pipeline)
+  var read =
+    pull(
+      pull.readArray(['billy', 'joe', 'zeke']),
+      pipeline
+    )
 
   t.equal('function', typeof read)
   //we will know it's a read function,
   //because read takes two args.
   t.equal(2, read.length)
 
-  read
-    .pipe(pull.writeArray(function (err, array) {
+  pull(
+    read,
+    pull.writeArray(function (err, array) {
       console.log(array)
       t.deepEqual(
         array, 
         [ '*** BILLY! ***', '*** JOE! ***', '*** ZEKE! ***' ]
       )
       t.end()
-    }))
+    })
+  )
 
 })
