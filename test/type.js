6621804adba3847f31dc0011eee7d23207f9524e
Jamen Marz <jamenmarz@gmail.com>
2016-11-07 02:50:42 -0700

    Remove checking through and fix tests.

--- test/type.js
+++ test/type.js
@@ -1,14 +1,14 @@
 var test = require('tape')
 var pull = require('../')
-var type = require('../util/type')
+var type = require('../type')
 
 test('util - type', function (t) {
   t.plan(6)
 
   t.is(type(pull.values()), 'source', 'type of pull.values()')
   t.is(type(pull.once()), 'source', 'type of pull.once()')
-  t.is(type(pull.map()), 'through', 'type of pull.map()')
-  t.is(type(pull.take()), 'through', 'type of pull.take()')
+  // t.is(type(pull.map()), 'through', 'type of pull.map()')
+  // t.is(type(pull.take()), 'through', 'type of pull.take()')
   t.is(type(pull.reduce()), 'sink', 'type of pull.reduce()')
   t.is(type(pull.log()), 'sink', 'type of pull.log()')
 })
--- type.js
+++ type.js
@@ -1,16 +1,7 @@
-function abort (err, cb) { return cb(true) }
-
 module.exports = function type (stream) {
-  // Test if it is a through first.
-  // If a source, calling it like this will break.
-  // So we wrap a try/catch:
-  try {
-    var source = stream(abort)
-    if (typeof source === 'function' && source.length === 2) return 'through'
-  } catch (e) {}
-
-  // Test other streams, and otherwise null.
-  if (stream.length === 1) return 'sink'
-  else if (stream.length === 2) return 'source'
-  else return null
+  switch (argument.length) {
+    case 1: return 'sink'
+    case 2: return 'source'
+    default: return null
+  }
 }
