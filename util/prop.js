c2f2cc87eaabe3aa443ebf046bbf5235b63d6e38
Christian Bundy <christianbundy@fraction.io>
2019-10-03 09:24:07 -0700

    Refactor `util/prop.js` to remove ternary operators

--- util/prop.js
+++ util/prop.js
@@ -1,9 +1,26 @@
 module.exports = function prop (key) {
-  return key && (
-    'string' == typeof key
-    ? function (data) { return data[key] }
-    : 'object' === typeof key && 'function' === typeof key.exec //regexp
-    ? function (data) { var v = key.exec(data); return v && v[0] }
-    : key
-  )
+  if (!key) {
+    // Key is falsy, return it as-is.
+    return key
+  }
+
+  if (typeof key === 'string') {
+    // Key is a string, use the default function.
+    return (data) => data[key]
+  }
+
+  if (typeof key !== 'object' && typeof key.exec === 'function') {
+    // Key is a regular expression, use a function to return the first match.
+    return (data) => {
+      const v = key.exec(data)
+      if (v) {
+        return v[0]
+      } else {
+        return v
+      }
+    }
+  }
+
+  // Key is hopefully a function, so we return it as-is.
+  return key
 }
