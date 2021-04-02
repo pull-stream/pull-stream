d2ccf04fa6253040da20761ba37d12b1183145a1
Timothy Bradley <fennasnogothrim@gmail.com>
2021-04-01 17:55:27 -0700

    Revert "Reduce anonymous function creation during iteration for edge cases"
    This reverts commit 38006447b94b9113998954f15b6b869313e09d9e.

--- throughs/flatten.js
+++ throughs/flatten.js
@@ -3,24 +3,15 @@
 var values = require('../sources/values')
 var once = require('../sources/once')
 
-function getAbortCb(read, abort, cb) {
-  return function(err) {
-    read(err || abort, cb)
-  }
-}
-function getChunkErrorCb(err, cb) {
-  return function() {
-    cb(err)
-  }
-}
-
 //convert a stream of arrays or streams into just a stream.
 module.exports = function flatten () {
   return function (read) {
     var _read
     return function (abort, cb) {
       if (abort) { //abort the current stream, and then stream of streams.
-        _read ? _read(abort, getAbortCb(read, abort, cb)) : read(abort, cb)
+        _read ? _read(abort, function(err) {
+          read(err || abort, cb)
+        }) : read(abort, cb)
       }
       else if(_read) nextChunk()
       else nextStream()
@@ -29,7 +20,10 @@ module.exports = function flatten () {
         _read(null, function (err, data) {
           if (err) {
             if (err === true) nextStream()
-            else read(true, getChunkErrorCb(err, cb))
+            else read(true, function(abortErr) {
+              // TODO: what do we do with the abortErr?
+              cb(err)
+            })
           }
           else cb(null, data)
         })
